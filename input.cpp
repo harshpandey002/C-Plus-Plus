ab37d46b4f13a3bd79b24b83c59c86835d46d894
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-05-03 12:14:05 +0530

    Input

---
+++ input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int amount1;
+  cout<<"Enter amount 1: ";
+  cin>>amount1;
+
+  int amount2;
+  cout<<"Enter amount 2: ";
+  cin>>amount2;
+
+  int sum = amount1 + amount2;
+  cout<<"Total amount: "<<sum;
+
+  return 0;
+}

34c5c01b9ab1690f74d7c924b9830795690ff5c7
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-05-03 11:53:07 +0530

    data types

---
+++ datatypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a; //declaration
+
+  a = 12; //initialization
+  cout<<"Size of int "<< sizeof(a) << endl;
+
+  float b;
+  cout<<"Size of float "<<sizeof(b) << endl;
+
+  char c;
+  cout<<"Size of char "<<sizeof(c) << endl;
+
+  bool d;
+  cout<<"Size of bool "<<sizeof(d) << endl;
+
+  return 0;
+}

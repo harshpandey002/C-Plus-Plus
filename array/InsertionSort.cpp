c45f0fe77cfab7edd05680838e6d986255c19e9f
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-07-07 22:48:32 +0530

    Insertion Sort

---
+++ array/InsertionSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n, t;
+  cin>>n;
+  int arr[n];
+
+  for(int i=0; i<n; i++){
+    cin>>arr[i];
+  }
+
+
+  for(int i=1; i<n; i++){
+
+    if(arr[i-1] > arr[i]){
+
+      t = arr[i];
+      int j=i-1;
+
+      while(arr[j]>t){
+        arr[j+1] = arr[j];
+        j--;
+      }
+
+      arr[j+1] = t;
+
+    }
+  }
+
+
+
+  for(int i=0; i<n; i++){
+    cout<<arr[i]<<" ";
+  }
+
+  return 0;
+}

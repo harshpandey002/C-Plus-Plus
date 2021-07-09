1e8ee606e755e880aeaa94e9fa2753f7edaf2d9a
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-07-09 21:10:42 +0530

    Sum of Sub-Array (optimized)

--- array/subArraySum.cpp
+++ array/subArraySum.cpp
@@ -1,30 +1,64 @@
-//Brute Force
-
 #include <iostream>
 using namespace std;
 
 int main(){
-  int n, s=0;
+  int n;
   cin>>n;
-  int arr[n];
+
+  int arr[n], s;
+  cin>>s;
 
   for(int i=0; i<n; i++){
     cin>>arr[i];
   }
 
-  int sum;
-  cin>>sum;
+
+
+/* BRUTE FORCE
+int sum=arr[0];
 
   for(int i=0; i<n; i++){
-    for(int j=i; j<n; j++){
-      s+=arr[j];
-      if(s == sum){
+    for(int j=i+1; j<n; j++){
+      if(sum == s){
         cout<<i<<" "<<j;
         return 0;
       }
+      sum+=arr[j];
     }
-    s=0;
+    sum=0;
   }
+
+*/
+
+  // OPTIMIZED
+  int i=0;
+  int first_idx=0;
+  int last_idx=0;
+  int sum=0;
+
+
+  for(i=0; i<=n; i++){
+
+    if(sum<s){
+      sum=sum + arr[i];
+      last_idx++;
+    }
+    else if(sum==s){
+      cout<<first_idx+1<<" "<<last_idx<<endl;
+      break;
+    }
+    else {
+      sum=sum-arr[first_idx];
+      first_idx++;
+      i--;
+    }
+  }
+  
+  if(i>n){
+    cout<<"Not found"<<endl;
+  }
+
+
 
 
   return 0;
